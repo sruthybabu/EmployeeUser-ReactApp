712291453b7eb8482213b647f327bbc7d8304ce2
sruthybabu <sruthybabu64@gmail.com>
2022-05-21 20:20:40 +0530

    header complete

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Login from './components/Login';
 import Adduser from './components/Adduser';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-<Login/>
-<Adduser/>
+<BrowserRouter>
+<Routes>
+  <Route path="/" exact element={<Login/>}/>
+  <Route path="/add" exact element={<Adduser/>}/>
+  
+</Routes>
+</BrowserRouter>
 
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
+  <div className="container-fluid">
+      <Link to="/" className="navbar-brand" >USER</Link>
+   
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+          <Link to="/" className="nav-link active">Login</Link>
+        <Link to="/add" className="nav-link">Add User</Link>   
+        
+      </div>
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default Header
