f4360e732e6a5289f21e3cc4290929f906e76758
sruthybabu <sruthybabu64@gmail.com>
2022-05-22 20:32:23 +0530

    viewuser completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Login from './components/Login';
 import Adduser from './components/Adduser';
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Viewuser from './components/Viewuser';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
 <Routes>
   <Route path="/" exact element={<Login/>}/>
   <Route path="/add" exact element={<Adduser/>}/>
+  <Route path="/view" exact element={<Viewuser/>}/>
   
 </Routes>
 </BrowserRouter>
